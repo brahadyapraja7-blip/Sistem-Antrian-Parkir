4b115ff0b88fb979949d8c816f059740e4677d4a
fr13nds <brahadyapraja7@gmail.com>
2026-04-23 22:42:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+# Sistem Antrian Parkir menggunakan Queue (Array)
 
+# Inisialisasi queue
+antrian_parkir = []
+
+# Kapasitas parkiran
+kapasitas = 5
+
+# Enqueue (kendaraan masuk)
+def enqueue(kendaraan):
+    if len(antrian_parkir) < kapasitas:
+        antrian_parkir.append(kendaraan)
+        print(f"Kendaraan {kendaraan} masuk ke parkiran.")
+    else:
+        print("Parkiran penuh! Tidak bisa menambah kendaraan.")
+
+# Dequeue (kendaraan keluar)
+def dequeue():
+    if len(antrian_parkir) > 0:
+        keluar = antrian_parkir.pop(0)
+        print(f"Kendaraan {keluar} keluar dari parkiran.")
+    else:
+        print("Parkiran kosong!")
